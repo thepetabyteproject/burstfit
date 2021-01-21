a8816eeeba45e9adae6806ae8a96e26f372e1f7b
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2021-01-21 13:33:39 -0500

    more logging, better handling of mask

--- burstfit/data.py
+++ burstfit/data.py
@@ -35,6 +35,7 @@ class BurstData(Candidate):
         )
 
     def prepare_data(self, mask_chans=[], time_window=200e-3, normalise=True):
+        logging.info("Preparing data for burst fitting.")
         self.get_chunk()
         nt, nf = self.data.shape
         self.i0 = nt // 2
@@ -61,18 +62,24 @@ class BurstData(Candidate):
         return self.tcand // self.tsamp - (nt // 2)
 
     def mask_channels(self, mask_chans=[]):
+        logging.debug(f"Masking channels")
         for m in mask_chans:
             if isinstance(m, tuple):
+                assert len(m) == 2
                 self.dedispersed.mask[:, m[0] : m[1]] = True
             elif isinstance(m, int):
                 self.dedispersed.mask[:, m] = True
+            elif isinstance(m, list):
+                assert len(m) == 2
+                self.dedispersed.mask[:, m[0] : m[1]] = True
             else:
                 raise AttributeError(
-                    "mask_chans can only contain tuple (start_chan:end_chan) and/or ints"
+                    "mask_chans can only contain tuple/list (start_chan:end_chan) and/or ints"
                 )
         return self
 
     def normalise_data(self, on_pulse_data, off_pulse_data, return_clip_fac=True):
+        logging.info(f"Normalising data using off pulse mean and std.")
         off_pulse_mean = np.mean(off_pulse_data)
         off_pulse_std = np.std(off_pulse_data)
         logging.info(f"Off pulse mean and std are: {off_pulse_mean, off_pulse_std}")
@@ -80,9 +87,11 @@ class BurstData(Candidate):
         on_pulse_data = on_pulse_data / off_pulse_std
         if return_clip_fac:
             clip_fac = ((2 ** self.nbits - 1) - off_pulse_mean) / off_pulse_std
+            logging.debug(f"Clip factor is {clip_fac}")
         return on_pulse_data, clip_fac
 
     def crop_dedispersed_data(self, time_window):
+        logging.info(f"Cropping data with time_window: {time_window}s.")
         time_around_burst = int(time_window // self.tsamp // 2)
         return self.dedispersed[
             self.i0 - time_around_burst : self.i0 + time_around_burst, :
