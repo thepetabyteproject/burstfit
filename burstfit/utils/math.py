4151973c71b755495ee46ff045928c9ef9a44ffd
Kshitij Aggarwal <kshitij.aggarwal@iitrpr.ac.in>
2021-02-05 14:09:32 -0500

    updated fma to fmae so it can handle errors. added fn to transform parameters

--- burstfit/utils/math.py
+++ burstfit/utils/math.py
@@ -56,15 +56,47 @@ def tests(off_pulse, on_pulse_res, pth=0.05, ntest=1):
         return 0
 
 
-def fma(param, m, a):
+def fmae(param, m, a, param_err=0, m_err=0, a_err=0):
     """
-
     Args:
         param: parameter value
         m: number to multiply to param
         a: number to add
+        param_err: error on the fitted parameter
+        m_err: error on multiplier
+        a_err: error on adder
+
+    Returns:
+    """
+    p = param * m
+    p_err = p * np.sqrt((param_err / param) ** 2 + (m_err / m) ** 2)
+
+    p = p + a
+    p_err = np.sqrt(p_err ** 2 + a_err ** 2)
+
+    return p, p_err
+
+
+def transform_parameters(params, mapping, param_names):
+    """
+    Transform parameters and their errors using mapping
+
+    Args:
+        params: dictionary with parameter values and errors in "popt" and "perr"
+        mapping: mapping for all parameters
+        param_names: names of parameters to be used to identify the correct mapping
 
     Returns:
 
     """
-    return param * m + a
+    assert len(mapping) == len(params['popt'])
+    assert len(mapping) == len(params['perr'])
+    physical_dict = {}
+    physical_dict['popt'] = {}
+    physical_dict['perr'] = {}
+    for key in mapping:
+        k, m, m_err, a, a_err = mapping[key]
+        param = params['popt'][param_names.index(k)]
+        err = params['perr'][param_names.index(k)]
+        physical_dict['popt'][key], physical_dict['perr'][key] = fmae(param, m, a, err, m_err, a_err)
+    return physical_dict
