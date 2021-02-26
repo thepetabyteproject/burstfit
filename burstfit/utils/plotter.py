fc823c95f22dfa02e8126223d3a1a32f951c11d3
Kshitij Aggarwal <kshitij.aggarwal@iitrpr.ac.in>
2021-02-26 16:44:30 -0500

    new plotting

--- burstfit/utils/plotter.py
+++ burstfit/utils/plotter.py
@@ -3,6 +3,8 @@ import os
 import matplotlib
 import pylab as plt
 import numpy as np
+from skimage.transform import resize
+
 matplotlib.use("Agg")
 
 
@@ -94,8 +96,10 @@ def plot_2d_fit(
     model = function([0], *popt)
     if len(model.shape) == 1:
         model = model.reshape(sgram.shape)
+
     vmin = sgram.min()
     vmax = sgram.max()
+
     diff = sgram - model
     nf, nt = sgram.shape
 
@@ -103,21 +107,26 @@ def plot_2d_fit(
     ts = l * tsamp * 1000
 
     fig, axes = plt.subplots(2, 2, figsize=(7, 7), sharex=True)
-    axes[0, 0].imshow(sgram, aspect="auto", vmin=vmin, vmax=vmax,
-                      extent=[ts[0], ts[-1], nf, 0])
+    axes[0, 0].imshow(
+        sgram, aspect="auto", vmin=vmin, vmax=vmax, extent=[ts[0], ts[-1], nf, 0]
+    )
     axes[0, 0].set_title("Original Spectrogram")
-    axes[0, 1].imshow(model, aspect="auto",
-                      extent=[ts[0], ts[-1], nf, 0])
+    axes[0, 1].imshow(model, aspect="auto", extent=[ts[0], ts[-1], nf, 0])
     axes[0, 1].set_title("Model")
-    axes[1, 0].imshow(diff, aspect="auto", vmin=vmin, vmax=vmax,
-                      extent=[ts[0], ts[-1], nf, 0])
+    axes[1, 0].imshow(
+        diff, aspect="auto", vmin=vmin, vmax=vmax, extent=[ts[0], ts[-1], nf, 0]
+    )
     axes[1, 0].set_title("Residual Spectrogram")
-    axes[1, 0].set_xlabel('Time (ms)')
-    axes[1, 1].plot(ts, sgram.mean(0), c="k", alpha=0.7, linestyle="--", label="Original")
-    axes[1, 1].plot(ts, diff.mean(0), c="r", alpha=0.7, linestyle='dotted', label="Residual")
+    axes[1, 0].set_xlabel("Time (ms)")
+    axes[1, 1].plot(
+        ts, sgram.mean(0), c="k", alpha=0.7, linestyle="--", label="Original"
+    )
+    axes[1, 1].plot(
+        ts, diff.mean(0), c="r", alpha=0.7, linestyle="dotted", label="Residual"
+    )
     axes[1, 1].legend()
     axes[1, 1].set_title("Profiles")
-    axes[1, 1].set_xlabel('Time (ms)')
+    axes[1, 1].set_xlabel("Time (ms)")
 
     if title:
         fig.suptitle(title)
@@ -128,7 +137,132 @@ def plot_2d_fit(
         plt.savefig(outdir + "/" + outname + ".png", bbox_inches="tight", dpi=300)
     if show:
         plt.show()
-    return fig
+
+
+def plot_fit_results(
+    sgram,
+    function,
+    popt,
+    tsamp,
+    fstart,
+    foff,
+    mask=None,
+    outsize=None,
+    title=None,
+    show=True,
+    save=False,
+    outname="2d_fit_res",
+    outdir=None,
+    vmin=None,
+    vmax=None
+):
+    """
+
+    Args:
+        sgram: Original spectrogram data
+        function: spectrogram function used for modeling
+        popt: parameters for function
+        tsamp: sampling time (s)
+        fstart: start frequency (MHz)
+        foff: channel bandwidth (MHz)
+        mask: channel mask array
+        outsize: resize the 2D plots
+        title: title of the plot
+        show: to show the plot
+        save: to save the plot
+        outname: output name of png file
+        outdir: output directory for the plot
+        vmin: minimum range of colormap
+        vmax: maximum range of colormap
+
+    Returns:
+
+    """
+    if np.any(outsize):
+        sgram[sgram.mask] = 0
+
+    model = function([0], *popt)
+    if len(model.shape) == 1:
+        model = model.reshape(sgram.shape)
+
+    if not vmin:
+        vmin = sgram.min()
+
+    if not vmax:
+        vmax = sgram.max()
+
+    nf, nt = sgram.shape
+    assert nf % outsize[0] == 0
+
+    freqs = fstart + foff * np.linspace(0, nf - 1, nf)
+    if not np.any(mask):
+        mask = np.zeros(len(freqs), dtype=np.bool)
+
+    if np.any(outsize):
+        assert len(outsize) == 2
+        tsamp = tsamp * nt / outsize[1]
+        freqs = freqs.reshape(outsize[0], nf // outsize[0]).mean(-1)
+        mask = (mask.reshape(outsize[0], nf // outsize[0]).mean(-1)).astype(np.bool)
+        model = resize(model, outsize)
+        sgram = resize(sgram, outsize)
+
+    diff = sgram - model
+    nf, nt = sgram.shape
+    l = np.linspace(-nt // 2, nt // 2, nt)
+    ts = l * tsamp * 1000
+
+    fig, axes = plt.subplots(2, 3, figsize=(10, 7), sharex=True, sharey="row")
+    kwargs = {}
+    kwargs["extent"] = [ts[0], ts[-1], freqs[-1], freqs[0]]
+    kwargs["vmin"] = vmin
+    kwargs["vmax"] = vmax
+    kwargs["aspect"] = "auto"
+
+    axes[0, 0].imshow(sgram, **kwargs)
+    axes[0, 0].set_title("Original")
+    axes[0, 1].imshow(model, aspect="auto", extent=[ts[0], ts[-1], freqs[-1], freqs[0]])
+    axes[0, 1].set_title("Model")
+    axes[0, 2].imshow(diff, **kwargs)
+    axes[0, 2].set_title("Residual")
+    for freq in zip(freqs[mask]):
+        for ax in axes[0, :]:
+            ax.axhline(freq, color="r", xmin=0, xmax=0.03, lw=0.1)
+
+    axes[1, 0].plot(
+        ts, sgram.mean(0), c="r", alpha=1, linestyle="solid", label="Original"
+    )
+    axes[1, 1].plot(
+        ts,
+        model.mean(0),
+        c="r",
+        alpha=1,
+        linestyle="solid",
+        label="Residual",
+    )
+    axes[1, 2].plot(
+        ts,
+        diff.mean(0),
+        c="r",
+        alpha=1,
+        linestyle="solid",
+        label="Residual",
+    )
+    axes[1, 1].set_ylim()
+    axes[1, 0].set_xlabel("Time (ms)")
+    axes[1, 1].set_xlabel("Time (ms)")
+    axes[1, 2].set_xlabel("Time (ms)")
+
+    if title:
+        fig.suptitle(title)
+    plt.tight_layout()
+    if save:
+        if not outdir:
+            outdir = os.getcwd()
+        plt.savefig(
+            outdir + "/" + outname + "fit_results.png", bbox_inches="tight", dpi=300
+        )
+    if show:
+        plt.show()
 
 
 def plot_me(datar, xlabel=None, ylabel=None, title=None):
