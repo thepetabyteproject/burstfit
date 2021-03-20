801bdad883523f413dd6e8844f82bab88571773a
Devansh Agarwal <devansh.kv@gmail.com>
2021-03-20 08:46:00 -0400

    Added notebooks
    Signed-off-by: Devansh Agarwal <devansh.kv@gmail.com>

--- build_docs.sh
+++ build_docs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 printf "\033[0;32mGenerating markdown for docs...\033[0m\n"
-pydoc-markdown
+pydoc-markdown || exit
+printf "\033[0;32mCopying example notebooks...\033[0m\n"
+cp -rp examples/* docs/build/content/ || exit
+cd docs || exit
 printf "\033[0;32mBuilding documentation site...\033[0m\n"
-cd docs
 mkdocs build
