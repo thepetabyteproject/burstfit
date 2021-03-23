7998d7cf79702373fc7b92c963e22a2c5a877edc
Kshitij Aggarwal <kshitij.aggarwal@iitrpr.ac.in>
2021-03-23 19:03:32 -0400

    better io tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -29,8 +29,8 @@ def bd():
 
 @pytest.fixture(scope="module", autouse=True)
 def bf(bd):
-    pm = Model(pulse_fn)
-    sm = Model(gauss_norm2)
+    pm = Model(pulse_fn, param_names=["S", "mu_t", "sigma_t", "tau"])
+    sm = Model(gauss_norm2, param_names=["mu1", "sig1", "mu2", "sig2", "amp1"])
     sgmodel = SgramModel(pm, sm, sgram_fn)
     bf = BurstFit(
         sgram_model=sgmodel,
@@ -44,6 +44,14 @@ def bf(bd):
     )
     bf.fitall(spectra_bounds=([50, 5, 200, 5, 0], [150, 50, 300, 50, 1]), plot=False)
     return bf
+
+
+@pytest.fixture(scope="module", autouse=True)
+def bio():
+    ref_json = os.path.join(_install_dir, "data/test.json")
+    bio = BurstIO(jsonfile=ref_json)
+    bio.read_json_and_precalc()
+    return bio
 
 
 def compare_res_dict(ref, d):
@@ -79,34 +87,62 @@ def test_ioclass(bf, bd):
 def test_set_attributes(bf, bd):
     bio = BurstIO(bf, bd)
     bio.set_attributes_to_save()
+    assert bio.sgram_params == bf.sgram_params
+    assert bio.ncomponents == 1
+    assert bio.sgram_function == "sgram_fn"
+    assert bio.pulse_function == "pulse_fn"
+    assert bio.spectra_function == "gauss_norm2"
+    assert bio.fileheader == vars(bd.your_header)
+    assert bio.param_names == [
+        "mu1",
+        "sig1",
+        "mu2",
+        "sig2",
+        "amp1",
+        "S",
+        "mu_t",
+        "sigma_t",
+        "tau",
+        "DM",
+    ]
+
+
+def test_save_results(bf, bd):
+    bio = BurstIO(bf, bd)
+    d = bio.save_results(outname="temp.json")
+    assert os.path.isfile("temp.json")
+    assert d["sgram_params"] == bf.sgram_params
+    assert d["ncomponents"] == bf.ncomponents
+    assert d["fileheader"] == vars(bd.your_header)
+    assert d["profile_bounds"] == bf.profile_bounds
+    assert d["spectra_bounds"] == bf.spectra_bounds
+    assert d["sgram_bounds"] == bf.sgram_bounds
+    assert d["clip_fac"] == bf.clip_fac
+    assert d["nt"] == bf.nt
+    assert d["nf"] == bf.nf
+    assert d["i0"] == bf.i0
+    os.remove("temp.json")
+
+
+def test_read_json_and_precalc(bf, bd, bio):
+    assert bio.sgram_params["all"]["1"]["popt"] == bf.sgram_params["all"][1]["popt"]
+    assert bio.sgram_params["all"]["1"]["perr"] == list(
+        bf.sgram_params["all"][1]["perr"]
+    )
     assert bio.ncomponents == 1
     assert bio.sgram_function == "sgram_fn"
     assert bio.pulse_function == "pulse_fn"
     assert bio.spectra_function == "gauss_norm2"
 
+    header = vars(bd.your_header)
+    for key, value in bio.fileheader.items():
+        if "base" not in key and "file" not in key:
+            assert value == header[key]
+    assert bio.clip_fac == bf.clip_fac
+    assert bio.nt == bf.nt
+    assert bio.nf == bf.nf
+    assert bio.i0 == bf.i0
 
-def test_read_json_and_precalc(bf):
-    ref_json = os.path.join(_install_dir, "data/28.json")
-    bio = BurstIO(jsonfile=ref_json)
 
-
-#  bio.read_json_and_precalc()
-#  assert bio.sgramModel.metadata == bf.metadata
-#  assert bio.sgramModel.param_names == bf.param_names
-
-
-# def test_save_results(bf, bd):
-#     bio = BurstIO(bf, bd)
-#     bio.save_results(outname='test.json')
-#     assert os.path.isfile('test.json')
-#
-#     with open('test.json', "r") as fp:
-#         d = json.load(fp)
-#
-#     ref_json = os.path.join(_install_dir, "data/28.json")
-#     with open(ref_json, "r") as fp:
-#         ref = json.load(fp)
-#
-#     compare_res_dict(ref, d)
-#
-#     os.remove("test.json")
+def test_model(bf, bio):
+    assert (bf.model - bio.model).sum() == 0
