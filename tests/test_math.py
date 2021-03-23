9751de9bbf6be7ab2e551217ee41345d07a649f3
Kshitij Aggarwal <kshitij.aggarwal@iitrpr.ac.in>
2021-03-23 17:59:35 -0400

    fixing and adding math tests

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-from burstfit.utils.math import f_test, fmae
+from burstfit.utils.math import f_test, fmae, transform_parameters
 from burstfit.utils.math import tests as te
 
 
 def test_ftest():
     a = np.random.normal(loc=0, scale=1, size=1000)
-    b = np.random.normal(loc=0, scale=1, size=1000)
-    f, p = f_test(a, b)
+    f, p = f_test(a, a)
     assert p > 0.1
 
     c = np.random.uniform(size=1000)
@@ -30,3 +29,38 @@ def test_fmae():
     r, e = fmae(2, 2, 1, 0, 0, 0)
     assert r == 5
     assert e == 0
+
+
+def test_transform_params():
+    params = {}
+    params["popt"] = [10, 20, 30]
+    params["perr"] = [1, 5, 6]
+    param_names = ["mu_f", "sigma_f", "S"]
+
+    mapping = {
+        "F_0": ["mu_f", 1.5, 0, 1400, 0],
+        "F_sig": ["sigma_f", 1.5, 0, 0, 0],
+        "Fluence": ["S", 2.5, 0.5, 0, 0],
+    }
+    physical_dict = transform_parameters(params, mapping, param_names)
+
+    assert (
+        physical_dict["popt"]["F_0"]
+        == params["popt"][0] * mapping["F_0"][1] + mapping["F_0"][3]
+    )
+    assert (
+        physical_dict["popt"]["F_sig"]
+        == params["popt"][1] * mapping["F_sig"][1] + mapping["F_sig"][3]
+    )
+    assert (
+        physical_dict["popt"]["Fluence"]
+        == params["popt"][2] * mapping["Fluence"][1] + mapping["Fluence"][3]
+    )
+
+    assert physical_dict["perr"]["F_0"] == params["perr"][0] * mapping["F_0"][1]
+    assert physical_dict["perr"]["F_sig"] == params["perr"][1] * mapping["F_sig"][1]
+
+    p = 30 * 2.5
+    p_err = p * np.sqrt((6 / 30) ** 2 + (0.5 / 2.5) ** 2)
+
+    assert physical_dict["perr"]["Fluence"] == p_err
